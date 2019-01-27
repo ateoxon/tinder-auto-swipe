b9ee69432cb796cb3c5c8602627cf4fca673fbb6
Angelo Teoxon <ateoxon97@gmail.com>
2019-01-27 03:50:11 -0600

    initial commit

---
+++ autoswipe.py
@@ -0,0 +1,10 @@
+# for those who are truly desperate - i wish you the best of luck
+# to be used on the web application where the right arrow key is "right swipe"
+
+import pyautogui
+import time
+
+
+while True:
+    time.sleep(1.0)
+    pyautogui.press("right")

1aefab288021d1633050389f452f9d231c5c6340
Angelo Teoxon <ateoxon97@gmail.com>
2019-01-30 11:58:03 -0600

    add things

--- autoswipe.py
+++ autoswipe.py
@@ -6,5 +6,8 @@ import time
 
 
 while True:
-    time.sleep(0.4)
+    time.sleep(0.5)
     pyautogui.press("right")
+
+    #implement function where pgm recognizes if swiping is not happening
+    #pyautogui.click(x=1183, y=621) #position is where "keep swiping shows up on screen"
